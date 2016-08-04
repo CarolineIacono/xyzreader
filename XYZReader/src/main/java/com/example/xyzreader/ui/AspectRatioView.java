2eef897759eaadb7e4d172ca642c050c570ccbae
Caroline <cstewart@mercury.io>
2016-08-04 12:01:30 -0500

    final commit

---
+++ XYZReader/src/main/java/com/example/xyzreader/ui/AspectRatioView.java
@@ -0,0 +1,18 @@
+package com.example.xyzreader.ui;
+
+import android.content.Context;
+import android.widget.ImageView;
+
+/**
+ * Created by carolinestewart on 7/14/16.
+ */
+public class AspectRatioView extends ImageView {
+    public AspectRatioView (Context context) {
+        super(context);
+    }
+
+//    public AspectRatioView (Context context, AttibuteSet attrs) {
+//
+//    }
+}
+
